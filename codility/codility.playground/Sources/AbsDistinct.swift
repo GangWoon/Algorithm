3f9f0d59f0996c3f0cbcdbb2f4281a13c7bb7c2a
gangwoon <gangwoon2018@gmail.com>
2024-10-23 15:02:27 +0900

    codility-AbsDistinct

---
+++ codility/codility.playground/Sources/AbsDistinct.swift
@@ -0,0 +1,21 @@
+
+
+
+public func AbsDistinctsolution(_ A : inout [Int]) -> Int {
+  var left = 0
+  var right = A.count - 1
+  var arr: Set<Int> = []
+  
+  while left <= right {
+    let leftValue = abs(A[left])
+    let rightValue = abs(A[right])
+    if leftValue > rightValue {
+      arr.insert(leftValue)
+      left += 1
+    } else {
+      arr.insert(rightValue)
+      right -= 1
+    }
+  }
+  return arr.count
+}
