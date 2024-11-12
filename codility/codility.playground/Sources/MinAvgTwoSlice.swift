b97519b52b3905f499257660910fca2a395df2ee
gangwoon <gangwoon2018@gmail.com>
2024-11-12 20:31:14 +0900

    codility-MinAvgTwoSlice

--- codility/codility.playground/Sources/MinAvgTwoSlice.swift
+++ codility/codility.playground/Sources/MinAvgTwoSlice.swift
@@ -38,6 +38,32 @@
 //N is an integer within the range [2..100,000];
 //each element of array A is an integer within the range [−10,000..10,000].
 
+
+/// 배열의 부분은 2, 3으로 이루어진다는 걸 잊지 않기.
+
+public func MinAvgTwoSlicesolution2(_ A : inout [Int]) -> Int {
+  var minValue = Double(Int.max)
+  var index = 0
+  
+  for i in 0..<(A.count - 1) {
+    let sum2 = Double(A[i] + A[i + 1]) / 2
+    if  sum2 < minValue {
+      index = i
+      minValue = sum2
+    }
+    
+    if i < (A.count - 2) {
+      let sum3 = Double((A[i] + A[i + 1] + A[i + 2])) / 3
+      if sum3 < minValue {
+        minValue = sum3
+        index = i
+      }
+    }
+  }
+  
+  return index
+}
+
 public func  MinAvgTwoSlicesolution(_ A : inout [Int]) -> Int {
   var minimum = Double.infinity
   var result = 0
