5698fd20a288e8c8d2a5cc7596c4880de4eb5a4c
gangwoon <gangwoon2018@gmail.com>
2024-11-16 17:13:36 +0900

    codility-Fish

--- codility/codility.playground/Sources/Fish.swift
+++ codility/codility.playground/Sources/Fish.swift
@@ -1,3 +1,46 @@
+public func Fishsolution2(_ A : inout [Int], _ B : inout [Int]) -> Int {
+  var stack: [Int] = []
+  var count = 0
+  
+  for i in 0..<A.count {
+    if B[i] == 1 {
+      stack.append(A[i])
+    } else {
+      while !stack.isEmpty {
+        if stack.last! > A[i] {
+          break
+        } else {
+          stack.removeLast()
+        }
+      }
+      
+      if stack.isEmpty {
+        count += 1
+      }
+    }
+  }
+
+  return count + stack.count
+}
+
+//public func solution(_ A : inout [Int], _ B : inout [Int]) -> Int {
+//  var weight = 0
+//  var count = 0
+//
+//  for i in 0..<A.count {
+//    if B[i] == 1 {
+//      weight = A[i]
+//    } else {
+//      if weight < A[i] {
+//        count += 1
+//        weight = 0
+//      }
+//    }
+//  }
+//
+//  return count
+//}
+
 
 public func Fishsolution(_ A : inout [Int], _ B : inout [Int]) -> Int {
   var upSide = 0
