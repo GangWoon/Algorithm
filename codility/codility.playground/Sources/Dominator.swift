2b3f373198a81c6ce603697d9eabf16edc7deb65
gangwoon <gangwoon2018@gmail.com>
2024-11-19 00:03:37 +0900

    codility-Dominator

--- codility/codility.playground/Sources/Dominator.swift
+++ codility/codility.playground/Sources/Dominator.swift
@@ -27,6 +27,56 @@
 
 /// 배열 순회할 때는 항상 조심히.// <- 이게 참 어려운듯
 
+/// 보일러 무어를 이용할 것, 내 방식에서 문제가 발생할수도 있었고, 공간 복잡도를 생각해보면 비효율적임.
+public func Dominatorsolution2(_ A : inout [Int]) -> Int {
+  let N = A.count
+  guard N > 0 else { return -1 }
+  
+  var candidate: Int?
+  var count = 0
+  
+  for num in A {
+    if count == 0 {
+      candidate = num
+      count = 1
+    } else if candidate == num {
+      count += 1
+    } else {
+      count -= 1
+    }
+  }
+  
+  /// 딕셔너리를 사용하지 않으면, 공간 복잡도를 획기적으로 줄일 수 있음.
+  /// 알고리즘을 풀 때, 왠만하면 딕셔너리를 피하려고 노력해봅시다.
+  guard let dominator = candidate else { return -1 }
+  let dominatorCount = A.filter { $0 == dominator }.count
+  if dominatorCount > N / 2 {
+    return A.firstIndex(of: dominator)!
+  }
+  
+  return -1
+}
+//public func solution(_ A : inout [Int]) -> Int {
+//  var arr: [Int: Int] = [:]
+//  var maxValue = 0
+//  var result = 0
+//
+//  for (index, num) in A.enumerated() {
+//    arr[num, default: 0] += 1
+//    if maxValue != num, maxValue < arr[num]! {
+//      result = index
+//      maxValue = arr[num]!
+//    }
+//    maxValue = max(maxValue, arr[num]!)
+//    if maxValue > A.count / 2 {
+//      return result
+//    }
+//  }
+//
+//  return -1
+//}
+
+
 public func Dominatorsolution(_ A : inout [Int]) -> Int {
   var count: [Int: Int] = [:]
   var dominator: Int?
