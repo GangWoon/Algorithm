042bc2f680af1faa1d92da4388c287e1b09ae5e3
gangwoon <gangwoon2018@gmail.com>
2024-11-29 13:03:11 +0900

    codility-MaxSliceSum

--- codility/codility.playground/Contents.swift
+++ codility/codility.playground/Contents.swift
@@ -1,1 +1,16 @@
-import Foundation
+public func solution(_ A : inout [Int]) -> Int {
+  var maxSum = A[0]
+  var currentSum = A[0]
+  
+  for i in 1..<A.count {
+    currentSum = max(A[i], currentSum + A[i])
+    maxSum = max(maxSum, currentSum)
+  }
+  
+  return maxSum
+}
+
+
+var a = [3, 2, -6, 4, 0]
+
+solution(&a)
