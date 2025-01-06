fdf7774ec83e5cb4c2831732b6f4351ff4e821b8
gangwoon <gangwoon2018@gmail.com>
2025-01-06 23:13:37 +0900

    codility-MinMaxDivision

--- codility/codility.playground/Sources/MinMaxDivision.swift
+++ codility/codility.playground/Sources/MinMaxDivision.swift
@@ -1,3 +1,49 @@
+/// 이진 검색을 풀 때는 사고를 조금 더 다양하게 접근해야 할 거 같음.
+/// 배열 A의 최대 값이 배열을 분리시켰을 때
+
+public func MinMaxDivisionsolution2(
+  _ K : Int,
+  _ M : Int,
+  _ A : inout [Int]
+) -> Int {
+  var left = A.max()!
+  var right = A.reduce(0, +)
+  var result = right
+  
+  while left <= right {
+    let mid = (left + right) / 2
+    if canDivide2(A, K, mid) {
+      result = mid
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
+  }
+  
+  return result
+}
+
+func canDivide2(_ A: [Int], _ K: Int, _ maxSum: Int) -> Bool {
+  var count = 1
+  var sum = 0
+  
+  for value in A {
+    if sum + value > maxSum {
+      count += 1
+      sum = value
+      if count > K {
+        return false
+      }
+    } else {
+      sum += value
+    }
+  }
+  
+  return true
+}
+
+
+
 /// 문제를 잘 읽고, 많이 풀어보자.
 
 public func MinMaxDivisionsolution(_ K : Int, _ M : Int, _ A : inout [Int]) -> Int {
