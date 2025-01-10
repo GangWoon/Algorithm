e21f39dad64ce0f63429216e8ec25ea8944abd47
gangwoon <gangwoon2018@gmail.com>
2025-01-10 23:25:58 +0900

    codility-CountTriangles

--- codility/codility.playground/Sources/CountTriangles.swift
+++ codility/codility.playground/Sources/CountTriangles.swift
@@ -1,3 +1,46 @@
+///규칙은
+///A[P] + A[Q] > A[R]
+///A[Q] + A[R] > A[P]
+///A[R] + A[P] > A[Q]
+///
+
+///범위
+///A.count ~= 0...1000
+/// 0 < ele < 1_000_000_000
+/// 배열을 받고나서, 3개 요소를 뽑을 수 있는 지 판별해야함.
+///
+///
+/// 생각
+/// 조합의 인덱스를 따지는게 아니니까 정렬을해도 문제가 없어보임.
+///
+/// 정렬을하게 되면 이득이 있을까? 의미가 있음. 정렬이 됐기 때문에 left, right로 접근이 가능해짐.
+/// 슬라이딩을 써야할 거 같음.
+///
+/// 정렬하게 되면 P < Q < R 순서로 배치된다.  A[P] + A[Q] > A[R]만 확인하면 된다. <- 여기까지 알아냄
+/// 역으로 접근해서 풀어야지 효율적일 거 같았음. <- 이 부분부터 설계를 못했음.
+
+public func CountTrianglessolution2(_ A : inout [Int]) -> Int {
+  A.sort()
+  let N = A.count
+  var count = 0
+  
+  for R in stride(from: N - 1, through: 2, by: -1) {
+    var P = 0
+    var Q = R - 1
+    
+    while P < Q {
+      if A[P] + A[Q] > A[R] {
+        count += Q - P
+        Q -= 1
+      } else {
+        P += 1
+      }
+    }
+  }
+  
+  return count
+}
+
 public func CountTrianglessolution(_ A: inout [Int]) -> Int {
   let N = A.count
   if N < 3 {
