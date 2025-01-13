498d3ba6c2decbbd591a17f2f178e85ca5e9741b
gangwoon <gangwoon2018@gmail.com>
2025-01-14 03:43:32 +0900

    codilty-MaxNonoverlappingSegments

--- codility/codility.playground/Sources/MaxNonoverlappingSegments .swift
+++ codility/codility.playground/Sources/MaxNonoverlappingSegments .swift
@@ -1,3 +1,22 @@
+// 망상하지 않기, 문제를 풀 떄 정확한 사실들만 나열하려고 노력해보자.
+// 문제 접근은 바로했지만 이상한 생각으로 인해서 틀림.
+public func MaxNonoverlappingSegmentssolution2(_ A : inout [Int], _ B : inout [Int]) -> Int {
+  let N = A.count
+  if N == 0 { return 0}
+
+  var result = 0
+  var lastEnd = -1
+  
+  for i in 0..<N {
+    if A[i] > lastEnd {
+      result += 1
+      lastEnd = B[i]
+    }
+  }
+  
+  return result
+}
+
 public func MaxNonoverlappingSegmentssolution(_ A : inout [Int], _ B : inout [Int]) -> Int {
   let N = A.count
   if N == 0 { return 0 }
